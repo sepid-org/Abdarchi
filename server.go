448e9b3f5381d691b8455db73e4e1734ab3ba732
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2024-02-24 02:52:24 +0330

    add dependence from http methods

--- server.go
+++ server.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
+	// "bytes"
+	// "encoding/json"
+	// "fmt"
 	"strings"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"net/http"
-	"io/ioutil"
+	"regexp"
+	// "io/ioutil"
 )
 
 // https://backend.kamva.academy/api/fsm/articles/?page=1
@@ -22,65 +23,49 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.POST("/post/", handlePost)
-	e.GET("/get/", handleGet)
-	e.GET("/test/:path*", SendReqGet)
+	
+	e.Any("/api/:path*", SendReqGet)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
-func handleGet(c echo.Context) error {
-	return c.String(http.StatusOK, "سلام فرمانده!")
-}
-
 func SendReqGet(c echo.Context) error {
 	// Retrieve the wildcard parameter
+	otherURL := ""
 	wildcardParam := c.Request().URL
 	wildcardParam1 := wildcardParam.String()
-	resultString := strings.Replace(wildcardParam1, "test/", "", 1)
-	fmt.Println("Wildcard parameter:", resultString)
-	if resultString == "/articles/?page=1"{
-	otherURL := "https://backend.kamva.academy/api/fsm/articles/?page=1"
-	resp, err := http.Get(otherURL)
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "Error making POST request to other URL")
-	}
-	defer resp.Body.Close()
-	responseBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-	    return c.String(http.StatusInternalServerError, "Error reading response body from other URL")
-	}
-	responseString := string(responseBody)
-	return c.String(http.StatusOK, responseString)
-	}
-	return c.String(http.StatusOK, "Wildcard parameter: "+"lk")
-}
+	re := regexp.MustCompile(`/api/([^/]+)`)
+	matches := re.FindStringSubmatch(wildcardParam1)
 
-func handlePost(c echo.Context) error {
-	var requestData RequestData
-	if err := c.Bind(&requestData); err != nil {
-		return c.String(http.StatusBadRequest, "Error parsing JSON request body")
+	if len(matches) > 1 {
+		firstWordAfterAPI := matches[1] 
+		if firstWordAfterAPI == "fsm"{
+			otherURL = "https://backend.kamva.academy/api"
+		}
+		if firstWordAfterAPI == "party-manager"{
+			otherURL = "https://mps.sepid.org/api"
+		}
+		// find service
 	}
-
-	data := requestData.Data
-
-	responseString := fmt.Sprintf("Received POST request with data: %s", data)
-
-	otherURL := "https://example.com/other-endpoint"
-	requestDataBytes, err := json.Marshal(requestData)
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "Error marshaling JSON data")
+	
+	resultString := strings.Replace(wildcardParam1, "api/", "", 1)
+	// fmt.Println("parameter:", resultString)
+	// fsm/articles/?page=1
+	if otherURL == ""{
+		return c.String(http.StatusOK, "سلام فرمانده!")
 	}
-
-	resp, err := http.Post(otherURL, "application/json", bytes.NewBuffer(requestDataBytes))
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "Error making POST request to other URL")
-	}
-	defer resp.Body.Close()
+	return c.Redirect(http.StatusMovedPermanently, otherURL + resultString)
+	// resp, err := http.Get(otherURL)
+	// if err != nil {
+	// 	return c.String(http.StatusInternalServerError, "Error making POST request to other URL")
+	// }
+	// defer resp.Body.Close()
 	// responseBody, err := ioutil.ReadAll(resp.Body)
 	// if err != nil {
 	//     return c.String(http.StatusInternalServerError, "Error reading response body from other URL")
 	// }
-	// responseString = string(responseBody)
-	return c.String(http.StatusOK, responseString)
+	// responseString := string(responseBody)
+	// return c.String(http.StatusOK, responseString)
+	// }
+	// return c.String(http.StatusOK, "Wildcard parameter: "+"lk")
 }
